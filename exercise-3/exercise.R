3b27a5f1415acc0d0b26899030190456eaeb1433
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2017-04-11 15:11:02 -0700

    Fin excercise 1 & 2

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,42 +2,67 @@
 
 # Load R's "USPersonalExpenditure" dataest using the `data()` function
 
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately, it's not a data.frame
 # Test this using the is.data.frame function
+
+is.data.frame(USPersonalExpenditure)
 
 
 # Luckily, you can simply pass the USPersonalExpenditure variable to the data.frame function
 # to convert it a data.farme
 
+data.frame(USPersonalExpenditure)
+
 # Create a new variable by passing the USPersonalExpenditure to the data.frame function
+
+new.v = data.frame(USPersonalExpenditure)
 
 
 # What are the column names of your dataframe?
+
+print(colnames(new.v))
 
 
 # Why are they so strange?
 
 
+
+
 # What are the row names of your dataframe?
+
+rownames(new.v)
 
 
 # Create a column `category` that is equal to your rownames
 
+new.v$ category = rownames(new.v)
+
 
 # How much money was spent on personal care in 1940?
+
+print(new.v["Personal Care", "X1940"])
 
 
 # How much money was spent on Food and Tobacco in 1960
 
+print(new.v["Food and Tobacco", "X1960"])
+
 
 # What was the highest expenditure category in 1960?
+
+print(max(new.v["X1960"]))
 
 
 ### Bonus ###
 
 # Write a function that takes in a year as a parameter, and 
 # returns the highest spending category of that year
+
+MaxSpending <- function(year) {
+  return (new.v$category (max(new.v[, year])))
+}
 
 # Using your function, determine the highest spending category of each year
 
